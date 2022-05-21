e185fbc710fae1fb9ab78bbfaef9a3ff9f28a442
Taren Sanders <taren.sanders@acu.edu.au>
2022-05-21 22:37:53 +1000

    Add tidy func for effects data

--- R/screenviz_app.R
+++ R/screenviz_app.R
@@ -8,8 +8,8 @@ screenviz_app <- function() {
   # Load and tidy the data
   df_effects <- load_data("Shared/screenviz/combined_effects.rds")
   df_reviews <- load_data("Shared/screenviz/reviews_raw.rds")
-  df_rob <- load_data("Shared/screenviz/rob_raw.rds")
   df_studies <- load_data("Shared/screenviz/studies_converted.rds")
+  df_rob <- load_data("Shared/screenviz/rob_raw.rds")
 
   df_effects <- tidy_data_effects(df_effects)
 
