b0431de3d098e612361e75741094b6a1c0bdacd2
Taren Sanders <taren.sanders@acu.edu.au>
2022-05-22 22:40:13 +1000

    load_data now caches data

--- R/load_data.R
+++ R/load_data.R
@@ -6,14 +6,27 @@
 #' @importFrom rlang .data
 #'
 #' @param path The location of the dataset on CloudStor
+#' @param use_cache Use a cached version of the dataset, if available?
 #' @export
 #' @return The loaded dataset
-load_data <- function(path) {
+load_data <- function(path, use_cache = TRUE) {
+  cache_path <- file.path("data", basename(path))
+
+  if (use_cache & file.exists(cache_path)) {
+    return(readRDS(cache_path))
+  }
+
   stopifnot(Sys.getenv("CLOUD_USER") != "", Sys.getenv("CLOUD_PASS") != "")
 
-  df <- cloudstoR::cloud_get(
+  dest <- ifelse(use_cache, cache_path, NULL)
+
+  df_path <- cloudstoR::cloud_get(
     path,
+    dest = dest,
     user = Sys.getenv("CLOUD_USER"),
-    password = Sys.getenv("CLOUD_PASS")
+    password = Sys.getenv("CLOUD_PASS"),
+    open_file = FALSE
   )
+
+  return(readRDS(df_path))
 }
