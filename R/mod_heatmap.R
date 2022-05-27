229ffb57b3feb79fb167fbd1e02d16b152770d20
Taren Sanders <taren.sanders@acu.edu.au>
2022-05-27 17:04:01 +1000

    WIP: mostly break things

--- R/mod_heatmap.R
+++ R/mod_heatmap.R
@@ -16,6 +16,8 @@ mod_heatmap_ui <- function(id) {
     shiny::textOutput(shiny::NS(id, "curr_outcome")),
     shiny::textOutput(shiny::NS(id, "clicked_out")),
     shiny::textOutput(shiny::NS(id, "out_level")),
+    shiny::tags$b("Data:"),
+    shiny::dataTableOutput(shiny::NS(id, "data"))
   )
 }
 
@@ -23,30 +25,82 @@ mod_heatmap_server <- function(id, data) {
   shiny::moduleServer(
     id,
     function(input, output, session) {
-      dataset <- shiny::reactive({
-        data$data
-      })
-      exposures_key <- shiny::reactive({
-        data$exp_key
-      })
-
-      outcomes_key <- shiny::reactive({
-        data$out_key
-      })
-
-      outcome_types <- c(
-        "outcome_category",
-        "outcome_group", "outcome_specific"
-      )
-      exposure_types <- c(
-        "exposure_group",
-        "exposure_specific"
-      )
+      # These objects are fixed
+      dataset <- data$data
+      exposures_key <- data$exp_key
+      outcomes_key <- data$out_key
+      outcome_types <- names(outcomes_key)
+      exposure_types <- names(exposures_key)
 
       # Instantiate variables
       clicked <- shiny::reactiveValues(exp = NULL, out = NULL)
       exp_level <- shiny::reactiveValues(current = 1, updated = 1)
       out_level <- shiny::reactiveValues(current = 1, updated = 1)
+      curr_exposure <- shiny::reactive(update_curr(
+        exposures_key, exposure_types, exp_level$current, clicked$exp,
+        exp_level$updated
+      ))
+      curr_outcome <- shiny::reactive(update_curr(
+        outcomes_key, outcome_types, out_level$current, clicked$out,
+        out_level$updated
+      ))
+
+      output$curr_outcome <- shiny::renderText(paste(
+        "curr_outcome:",
+        paste(curr_outcome(), collapse = ", ")
+      ))
+
+      output$curr_exposure <- shiny::renderText(paste(
+        "curr_exposure:",
+        paste(curr_exposure(), collapse = ", ")
+      ))
+
+      output$clicked_exp <- shiny::renderText(paste(
+        "clicked_exp:",
+        clicked$exp
+      ))
+      output$clicked_out <- shiny::renderText(paste(
+        "clicked_out:",
+        clicked$out
+      ))
+
+      output$exp_level <- shiny::renderText(paste(
+        "exp_level:",
+        "current:", exp_level$current,
+        "updated:", exp_level$updated
+      ))
+      output$out_level <- shiny::renderText(paste(
+        "out_level:",
+        "current:", out_level$current,
+        "updated:", out_level$updated
+      ))
+    }
+  )
+}
+
+mod_heatmap_server_old <- function(id, data) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+      # These objects are fixed
+      dataset <- data$data
+      exposures_key <- data$exp_key
+      outcomes_key <- data$out_key
+      outcome_types <- names(outcomes_key)
+      exposure_types <- names(exposures_key)
+
+      # Instantiate variables
+      clicked <- shiny::reactiveValues(exp = NULL, out = NULL)
+      exp_level <- shiny::reactiveValues(current = 1, updated = 1)
+      out_level <- shiny::reactiveValues(current = 1, updated = 1)
+      curr_exposure <- shiny::reactive(update_curr(
+        exposures_key, exposure_types, exp_level$current, clicked$exp,
+        exp_level$updated
+      ))
+      curr_outcome <- shiny::reactive(update_curr(
+        outcomes_key, outcome_types, out_level$current, clicked$out,
+        out_level$updated
+      ))
 
       # Set observation logic
       shiny::observeEvent(input$xaxis_clicked, {
@@ -57,6 +111,10 @@ mod_heatmap_server <- function(id, data) {
         exp_level$current <- exp_level$updated
         exp_level$updated <- update_level(
           exposure_types, exp_level$current, clicked$exp
+        )
+        curr_exposure <- update_curr(
+          exposures_key, exposure_types, exp_level$current, clicked$exp,
+          exp_level$updated
         )
       })
 
@@ -69,6 +127,10 @@ mod_heatmap_server <- function(id, data) {
         out_level$updated <- update_level(
           outcome_types, out_level$current, clicked$out
         )
+        curr_outcome <- update_curr(
+          outcomes_key, outcome_types, out_level$current, clicked$out,
+          out_level$updated
+        )
       })
 
       shiny::observeEvent(input$heatmap_clicked_data, {
@@ -79,6 +141,10 @@ mod_heatmap_server <- function(id, data) {
           exp_level$updated <- update_level(
             exposure_types, exp_level$current, clicked$exp
           )
+          curr_exposure <- update_curr(
+            exposures_key, exposure_types, exp_level$current, clicked$exp,
+            exp_level$updated
+          )
         }
 
         if (is.null(clicked$out)) {
@@ -86,6 +152,10 @@ mod_heatmap_server <- function(id, data) {
           out_level$current <- out_level$updated
           out_level$updated <- update_level(
             outcome_types, out_level$current, clicked$out
+          )
+          curr_outcome <- update_curr(
+            outcomes_key, outcome_types, out_level$current, clicked$out,
+            out_level$updated
           )
         }
 
@@ -95,6 +165,10 @@ mod_heatmap_server <- function(id, data) {
           exp_level$updated <- update_level(
             exposure_types, exp_level$current, clicked$exp
           )
+          curr_exposure <- update_curr(
+            exposures_key, exposure_types, exp_level$current, clicked$exp,
+            exp_level$updated
+          )
         }
         if (clicked$out != input$heatmap_clicked_data$value[2]) {
           clicked$out <- input$heatmap_clicked_data$value[2]
@@ -102,27 +176,33 @@ mod_heatmap_server <- function(id, data) {
           out_level$updated <- update_level(
             outcome_types, out_level$current, clicked$out
           )
+          curr_outcome <- update_curr(
+            outcomes_key, outcome_types, out_level$current, clicked$out,
+            out_level$updated
+          )
         }
       })
 
-      curr_outcome <- shiny::reactive({
-        update_curr(
-          outcomes_key(), outcome_types, out_level$current, clicked$out,
-          out_level$updated
-        )
-      })
-      curr_exposure <- shiny::reactive({
-        update_curr(
-          exposures_key(), exposure_types, exp_level$current, clicked$exp,
-          exp_level$updated
-        )
-      })
+      # curr_outcome <- shiny::reactive({
+      #   update_curr(
+      #     outcomes_key, outcome_types, out_level$current, clicked$out,
+      #     out_level$updated
+      #   )
+      # })
+      # curr_exposure <- shiny::reactive({
+      #   update_curr(
+      #     exposures_key, exposure_types, exp_level$current, clicked$exp,
+      #     exp_level$updated
+      #   )
+      # })
 
       plot_data <- shiny::reactive({
-        dataset() %>%
+        dataset %>%
           dplyr::filter(.data$outcome %in% curr_outcome() &
             .data$exposure %in% curr_exposure())
       })
+
+      output$data <- shiny::renderDataTable(plot_data())
 
       output$heatmap <- echarts_heatmap(
         plot_data(),
