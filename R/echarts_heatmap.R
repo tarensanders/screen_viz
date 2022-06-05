7ffb3a092164a9748c198b53867321beebc6ef56
Taren Sanders <Taren.Sanders@acu.edu.au>
2022-06-05 22:15:52 +1000

    Add first pass of tooltip
    Needs formatting work.
    part of Style changes to heatmap #27

--- R/echarts_heatmap.R
+++ R/echarts_heatmap.R
@@ -3,9 +3,15 @@ echarts_heatmap <- function(plot_data, curr_outcome, curr_exposure, settings) {
     dplyr::mutate(
       exposure = stringr::str_wrap(.data$exposure, settings$wrap_width_x),
       outcome = stringr::str_wrap(.data$outcome, settings$wrap_width_y),
+      tooltip = glue::glue(
+        "<strong>Correlation:</strong> {format(r, digits = 2)}<br>",
+        "[{format(r_lb, digits = 2)} - {format(r_ub, digits = 2)}]<br>",
+        "<small>Click for more details</small>"
+      )
     ) %>%
     echarts4r::e_charts_("exposure") %>%
     echarts4r::e_heatmap_("outcome", "r",
+      bind = "tooltip",
       itemStyle = list(
         emphasis = list(shadowBlur = 10),
         borderColor = "#606060",
@@ -22,6 +28,12 @@ echarts_heatmap <- function(plot_data, curr_outcome, curr_exposure, settings) {
       orient = "horizontal",
       text = list("Risk", "Benefit"),
       textStyle = list(color = "#FFFFFF")
+    ) %>%
+    echarts4r::e_tooltip(
+      formatter = htmlwidgets::JS("
+      function(params){
+        return(params.name)}
+    ")
     ) %>%
     echarts4r::e_x_axis(
       axisLabel = list(
