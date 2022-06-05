7d194eb700a3ef109d072ddf71b93d2ab3cc0a9b
Taren Sanders <Taren.Sanders@acu.edu.au>
2022-06-05 21:35:38 +1000

    Nvm... don't need push and pop

--- tests/testthat/test-utils-general.R
+++
@@ -1,8 +0,0 @@
-test_that("'pop' and 'push' work as expected", {
-  test_obj <- c()
-  expect_equal(push(test_obj, "a"), list("a"))
-  expect_equal(push(test_obj, "b"), list("a", "b"))
-  expect_equal(pop(test_obj), "b")
-  expect_equal(pop(test_obj), "a")
-  expect_null(pop(test_obj))
-})
